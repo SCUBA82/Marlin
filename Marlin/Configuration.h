e9f0f59de6aef6435920031465aadc4a9620165a
Christian Thalhammer <christian_thalhammer@gmx.at>
2013-01-24 11:55:19 +0100

    Configuration option for I2C display - set I2C address

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -312,6 +312,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 //#define ULTIMAKERCONTROLLER //as available from the ultimaker online store.
 //#define ULTIPANEL  //the ultipanel as on thingiverse
+//#define ULTIPANEL_I2C 0x20 //I2C Display support. Configure your I2C address PCF8574: 0x20 - 0x27 Tested with PCF8574(A) I2C Portexpander using LiquidCrystal_I2C
 
 // The RepRapDiscount Smart Controller (white PCB)
 // http://reprap.org/wiki/RepRapDiscount_Smart_Controller
